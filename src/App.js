e8b2ff8f58bf7133d3788ca0c8af71fc14928320
sg53808283 <sg53808283@gmail.com>
2024-03-09 22:58:08 +0900

    내용 구성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
-import Layout from './components/Layout';
+import Home from './components/home/Home.jsx';
+import Sidebar from './components/sidebar/Sidebar.jsx'
+import About from './components/about/About.jsx';
+import Services from './components/services/Services.jsx';
+import Resume from './components/resume/Resume.jsx';
+import Portfolio from './components/portfolio/Portfolio.jsx';
+import Blog from './components/blog/Blog.jsx';
+import Contact from './components/contact/Contact.jsx';
 
 function App() {
   return (
-    <Layout></Layout>
+    <>
+    <Sidebar/>
+    <main className='main'> 
+      <Home/>
+      <About/>
+      <Services/>
+      <Resume/>
+      <Portfolio/>
+      <Blog/>
+      <Contact/>
+    </main>
+    </>
   );
 }
 
