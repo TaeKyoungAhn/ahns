6c94e1ec036777a0ed2f16d09607404a04cfaa21
TaeKyoungAhn <sg53808283@gmail.com>
2024-03-12 09:55:57 +0900

    firebase 변경사항 업데이트

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,11 +4,14 @@ import './home.css';
 
 const Home = () => {
     return (
-        <section className="home container" id="home">
-            <div className="intro">
-                <img src="" alt="" className="home__img" />
-            </div>
-            </section>
+        // <section className="home container" id="home">
+        //     <div className="intro">
+        //         <img src="" alt="" className="home__img" />
+        //     </div>
+        //     </section>
+        <div>
+            <h1>Home</h1>
+        </div>
     );
 }
 
