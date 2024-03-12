d8e394df83a3f106ccbde22176f43d3ac10c7a5d
TaeKyoungAhn <sg53808283@gmail.com>
2024-03-12 17:33:47 +0900

    Social 내용 추가

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -2,7 +2,23 @@ import React from "react";
 
 const HeaderSocials = () => {
     return(
-        <div></div>
+        <div className="home__socials">
+            <a herf="" className="home__social-link" target="_blank">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+
+            <a herf="" className="home__social-link" target="_blank">
+                <i class="fa-brands fa-github"></i>
+            </a>
+
+            <a herf="" className="home__social-link" target="_blank">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+
+            <a herf="" className="home__social-link" target="_blank">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+        </div>
     )
 }
 
