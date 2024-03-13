f34b368d5d5b347a41247de1a4ff14b80e1cee06
TaeKyoungAhn <sg53808283@gmail.com>
2024-03-13 16:53:10 +0900

    추가 내용 업데이트

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,7 +6,8 @@ const About = () => {
     return (
         <section className="about container section" id="about">
             <h2 className="section__title">About Me</h2>
-            <div className="about__container grid">
+
+            <div className="about container grid">
                 <img src={image} alt="" className="about__img" />
             </div>
         </section>
