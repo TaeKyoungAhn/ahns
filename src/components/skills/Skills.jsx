ca4a8f1143c050fbfa743f231007160e6d5f8ca4
TaeKyoungAhn <sg53808283@gmail.com>
2024-03-15 17:38:36 +0900

    skills 부분 구현

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./skills.css";
 import SkillData from "./Data";
+import {Pagination} from 'swiper/modules';
+import {Swiper , SwiperSlide} from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 
 const Skills = () => {
@@ -8,20 +14,28 @@ const Skills = () => {
         <section className="skills container section" id="skills">
             <h2 className="section__title">Skills</h2>
 
-            <div className="skills__container grid">
+            <Swiper className="skills__container grid"
+            modules={[Pagination]}
+            spaceBetween={30}
+            slidesPerView={1}
+            loop={true}
+            grabCursor={true}
+            pagination={{clickable: true}}
+           
+            >
                 {SkillData.map(({id, image, title, subtitle, comment}) => {
                     return(
-                        <div className="skills__item" key={id}>
+                        <SwiperSlide className="skills__item" key={id}>
                             <div className="thumb">
                                 <img src={image} alt="" className="skills__img"/>
                             </div>
                             <h3 className="skills__title">{title}</h3>
                             <span className="skills__subtitle">{subtitle}</span>
                             <div className="skills__comment">{comment}</div>
-                            </div>
+                            </SwiperSlide>
                     )
                 })}
-                </div>
+                </Swiper>
 
             </section>
     );
