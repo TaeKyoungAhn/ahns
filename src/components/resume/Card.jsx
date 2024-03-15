5fbbc9f079d9085789669fdfb1f4e164808af724
TaeKyoungAhn <sg53808283@gmail.com>
2024-03-15 15:50:11 +0900

    오타 수정

--- src/components/resume/Card.jsx
+++ src/components/resume/Card.jsx
@@ -4,7 +4,7 @@ const Card =(props) => {
     return(
         <div className="timeline__item">
             <i className={props.icon}></i>
-            <span className="timeline__date">{props.yea}</span>
+            <span className="timeline__date">{props.year}</span>
             <h3 className="timeline__title">{props.title}</h3>
             <p className="timeline__text">{props.desc}</p>
         </div>
