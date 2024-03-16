a53ba252da3c56a2c9037d21c462e3cc9b85f1cd
sg53808283 <sg53808283@gmail.com>
2024-03-16 12:16:11 +0900

    mediaquary 적용

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sidebar.css';
 import Logo from '../../assets/Logo.png';
 
 const Sidebar = () => {
+    const [toggle,showMenu] = useState(false);
+
     return (
-        <aside className='aside'>
-        <a href='#home' className='nav__logo'>
+        <>
+        <aside className={toggle ? "aside show-menu": "aside"}>
+        <a href='#Home' className='nav__logo'>
             <img src={Logo} alt="" />
         </a>
 
@@ -13,37 +16,32 @@ const Sidebar = () => {
             <div className='nav__menu'>
                 <ul className='nav__list'>
                     <li className='nav__item'>
-                        <a href='#home' className='nav__link'>
+                        <a href='#Home' className='nav__link'>
                             <i className="icon-home"></i>
                         </a>
                     </li>
                     <li className='nav__item'>
-                        <a href='#about' className='nav__link'>
+                        <a href='#About' className='nav__link'>
                             <i className="icon-user"></i>
                         </a>
                     </li>
                     <li className='nav__item'>
-                        <a href='#services' className='nav__link'>
+                        <a href='#Skills' className='nav__link'>
                             <i className="icon-wrench"></i>
                         </a>
                     </li>
                     <li className='nav__item'>
-                        <a href='#resume' className='nav__link'>
+                        <a href='#Portfolio' className='nav__link'>
                             <i className="icon-briefcase"></i>
                         </a>
                     </li>
                     <li className='nav__item'>
-                        <a href='#portfolio' className='nav__link'>
+                        <a href='#Resume' className='nav__link'>
                             <i className="icon-doc"></i>
                         </a>
                     </li>
                     <li className='nav__item'>
-                        <a href='#skills' className='nav__link'>
-                            <i className="icon-energy"></i>
-                        </a>
-                    </li>
-                    <li className='nav__item'>
-                        <a href='#contact' className='nav__link'>
+                        <a href='#Contact' className='nav__link'>
                             <i className="icon-phone"></i>
                         </a>
                     </li>
@@ -55,7 +53,13 @@ const Sidebar = () => {
             <span className="copyright">&copy; 2024 - 2025.</span>
         </div>
         </aside>
+
+        <div className={toggle ? "nav__toggle nav__toggle-open" : "nav__toggle"} 
+        onClick={() => showMenu(!toggle)}>
+            <i className="icon-menu"></i>
+        </div>
+        </>
     );
-}
+};
 
 export default Sidebar;
