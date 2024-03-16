136d7741c4187f459ac8040d12bd3134f08e26de
sg53808283 <sg53808283@gmail.com>
2024-03-16 12:28:20 +0900

    대소문자 재수정

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
     return (
         <>
         <aside className={toggle ? "aside show-menu": "aside"}>
-        <a href='#Home' className='nav__logo'>
+        <a href='#home' className='nav__logo'>
             <img src={Logo} alt="" />
         </a>
 
@@ -16,32 +16,37 @@ const Sidebar = () => {
             <div className='nav__menu'>
                 <ul className='nav__list'>
                     <li className='nav__item'>
-                        <a href='#Home' className='nav__link'>
+                        <a href='#home' className='nav__link'>
                             <i className="icon-home"></i>
                         </a>
                     </li>
                     <li className='nav__item'>
-                        <a href='#About' className='nav__link'>
+                        <a href='#about' className='nav__link'>
                             <i className="icon-user"></i>
                         </a>
                     </li>
                     <li className='nav__item'>
-                        <a href='#Skills' className='nav__link'>
+                        <a href='#skills' className='nav__link'>
                             <i className="icon-wrench"></i>
                         </a>
                     </li>
                     <li className='nav__item'>
-                        <a href='#Portfolio' className='nav__link'>
+                        <a href='#portfolio' className='nav__link'>
                             <i className="icon-briefcase"></i>
                         </a>
                     </li>
                     <li className='nav__item'>
-                        <a href='#Resume' className='nav__link'>
+                        <a href='#skills' className='nav__link'>
+                            <i className="icon-energy"></i>
+                        </a>
+                    </li>
+                    <li className='nav__item'>
+                        <a href='#resume' className='nav__link'>
                             <i className="icon-doc"></i>
                         </a>
                     </li>
                     <li className='nav__item'>
-                        <a href='#Contact' className='nav__link'>
+                        <a href='#contact' className='nav__link'>
                             <i className="icon-phone"></i>
                         </a>
                     </li>
