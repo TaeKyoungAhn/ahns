14975cf002cae554a05a4a2b9ad6922fc20d7cd5
TaeKyoungAhn <sg9956@naver.com>
2024-04-11 16:23:06 +0900

    target 삭제

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -38,7 +38,7 @@ const Portfolio = () => {
 
                             <span className="work__category">{category}</span>
                             <h3 className="work__title">{title}</h3>
-                            <a href={url} target="_blank" className="work__button">
+                            <a href={url} className="work__button">
                                 <i className="icon-link work__button-icon"></i>
                             </a>
 
