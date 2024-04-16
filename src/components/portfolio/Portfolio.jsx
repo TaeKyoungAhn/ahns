b6791b192bd4f82110076149c47ca589cb3800b4
sg53808283 <sg53808283@gmail.com>
2024-04-16 17:54:16 +0900

    url 새창에서 열기

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -38,7 +38,7 @@ const Portfolio = () => {
 
                             <span className="work__category">{category}</span>
                             <h3 className="work__title">{title}</h3>
-                            <a href={url} className="work__button">
+                            <a href={url} className="work__button" target="__blink">
                                 <i className="icon-link work__button-icon"></i>
                             </a>
 
